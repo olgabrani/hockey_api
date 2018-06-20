e465e9ed0b709b1cc9e4e308d9e0eedeb6a424a7
Olga Brani <olgabrani78@gmail.com>
2018-06-20 17:31:21 +0300

    Make app port and api_prefix configurable

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+  port: 3000,
+  api_prefix: 'api',
+};
+
+module.exports = config;
--- server/index.js
+++ server/index.js
@@ -2,25 +2,28 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const team_controller = require('./controllers/team')
 const player_controller = require('./controllers/player')
+const config = require('../config');
+
+
+const port =  process.env.PORT || config.port || 3000
+const api_prefix =  process.env.API_PREFIX || config.api_prefix || 'api'
 
 const app = express()
 app.use(bodyParser.json());
 
 
-
 // Team routes
-app.post('/api/teams', team_controller.create_team)
-app.get('/api/teams', team_controller.list_teams)
-app.get('/api/teams/:teamId?', team_controller.team_details)
+app.post(`/${api_prefix}/teams`, team_controller.create_team)
+app.get(`/${api_prefix}/teams`, team_controller.list_teams)
+app.get(`/${api_prefix}/teams/:teamId?`, team_controller.team_details)
 
 // Player routes
-app.post('/api/players', player_controller.create_player)
-app.get('/api/players', player_controller.list_players)
-app.get('/api/players/:playerId?', player_controller.player_details)
+app.post(`/${api_prefix}/players`, player_controller.create_player)
+app.get(`/${api_prefix}/players`, player_controller.list_players)
+app.get(`/${api_prefix}/players/:teamId?`, player_controller.player_details)
 
 
 
-const port = 3000
 app.listen(port, () => {
-    console.log(`Running on http://localhost:${port}`)
+  console.log(`Server is running on ${port}`)
 })
